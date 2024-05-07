b04bddeb96d6efd6173d62077fbe902d90afae8f
Stassy Bett <100481391+StassyB@users.noreply.github.com>
2024-05-07 21:02:18 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'header.php'; ?>
+<link rel="stylesheet" href="style.css">
+
+<section class="hero" >
+        <body><div class="hero-content">
+ <div>    
+         <img src="img/s2.jpg" >
+        </div>
+       
+         <h1>Empower Your Sexual Health Journey</h1>
+        <h2>Access Education, Consultation and Products</h2>
+        <a href="services.php" class="cta-btn">Get Started</a>
+   
+    </div></body>
+</section>
+
+
+<?php include 'footer.php'; ?>
